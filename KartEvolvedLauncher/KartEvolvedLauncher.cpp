64d685158273874904915badbfcc30e173978e38
Stardusstt <40061990+Stardusstt@users.noreply.github.com>
2018-06-10 14:35:33 +0800

    Add files via upload
    KartEvolvedLauncher

---
+++ KartEvolvedLauncher/KartEvolvedLauncher.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std ;
+int main () {
+	int choice ; 
+	
+	cout << endl << " v1.0	By Stardusstt" << endl << endl  ;
+	system("pause");
+	cout << endl ;
+	cout << " Start PPServer " << endl << endl ;
+	system("start PPServer.exe");
+	Sleep(2000);
+	cout << " Start KartRider Evolved " << endl << endl ;
+	system("start kartrider.exe");
+	system("pause");
+	system("cls");
+	
+	while(1){
+		
+		
+		cout << endl ;
+		cout << " 1 (Restart All)  2 (Restart Kartrider)  3 (Close)  4 (Exit) : " ;
+		cin >> choice ;
+		cout << endl ;
+			
+		if ( choice == 1 ){
+		    cout << " Start PPServer " << endl << endl ;
+			system("start PPServer.exe");
+			Sleep(2000);
+		    cout << " Start KartRider Evolved " << endl << endl ;
+			system("start kartrider.exe");
+			system("pause");
+			system("cls");
+			continue ;
+			
+		}
+		
+		if ( choice == 2 ){
+		    system("taskkill /f /im AdBalloonExt.exe");
+			cout << endl << " Start KartRider Evolved " << endl << endl ;
+			system("start kartrider.exe");
+			system("pause");
+			system("cls");
+			continue ;
+			
+		}
+		
+		if ( choice == 3 ){
+		    cout << endl << " Close KartRider Evolved " << endl  ;
+			system("taskkill /f /im kartrider.exe");
+			Sleep(500);
+			cout << endl << " Close PPServer " << endl  ;
+			system("taskkill /f /im PPServer.exe");
+			Sleep(500);
+			cout << endl << " Close AdBalloonExt " << endl   ;
+			system("taskkill /f /im AdBalloonExt.exe");
+			cout << endl ;
+			system("pause");
+			system("cls");
+			continue ;
+			
+		}
+		
+		if ( choice == 4 ){
+		    break ;
+		    
+		}
+		    
+	}
+	
+	
+	
+	
+} 
